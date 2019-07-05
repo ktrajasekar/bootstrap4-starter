7ca04cbdb558f609c3d01e4902238b2b03e81ba5
Rajasekar T <kt.rajasekar@gmail.com>
2019-07-06 00:01:55 +0530

    Clean up and few lib

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+      $(document).ready(function(){
+      	// Navigation Init
+    $("#navigation").navigation({
+      mobileBreakpoint:"768",
+       mobileBreakpoint:"1024",
+    });
+
+    // AOS Animation 
+		AOS.init();
+
+});
